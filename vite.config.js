c4c6e5fa31e9ed18a02cefd38b6be77d58ddbded
TRAORE LACINA <traorelac01@gmail.com>
2026-03-16 14:47:46 +0000

    mis a jour

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      // Redirige /api/* → Laravel (ajustez le port si besoin)
-      '/api': {
-        target: 'https://shocked-sharla-freelence-c2692768.koyeb.app/',
-        changeOrigin: true,
-        secure: false,
-      },
-      // Redirige /storage/* → fichiers publics Laravel
-      '/storage': {
-        target: 'https://shocked-sharla-freelence-c2692768.koyeb.app/',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  const target =
+    mode === 'production'
+      ? 'https://shocked-sharla-freelence-c2692768.koyeb.app'
+      : 'http://localhost:8000'
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: target,
+          changeOrigin: true,
+          secure: false,
+        },
+        '/storage': {
+          target: target,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  }
 })
